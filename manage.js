c77090e2c4e80deb7de26289b07ce556651dc661
sekisaii <145209193+sathirak@users.noreply.github.com>
2024-04-24 21:43:53 +0530

    fix: added sort by newest first

--- manage.js
+++ manage.js
@@ -15,7 +15,7 @@ function run(result) {
 
 		cargo.forEach((item, index) => {
 
-			cargo_list += `<li class="cargo-item">
+			let cargo_list_block = `<li class="cargo-item">
 
                                 <div>
                                     <a target="_blank" href="${item.link}">
@@ -58,6 +58,8 @@ function run(result) {
                                         <svg xmlns="http://www.w3.org/2000/svg" style="display:block;" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-settings-2"><path d="M20 7h-9"/><path d="M14 17H5"/><circle cx="17" cy="17" r="3"/><circle cx="7" cy="7" r="3"/></svg>
                                     </button>
                            </li>`;
+
+                           cargo_list = cargo_list_block + cargo_list;
 
 		});
 
